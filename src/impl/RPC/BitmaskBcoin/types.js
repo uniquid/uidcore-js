8bde35f8626af7949fd0264f1c4f5386701481b9
aleclofabbro <alessandro.giansanti@gmail.com>
2018-10-03 11:24:21 +0200

    fix(package): cleanup mistaken/stale js files in src

--- src/impl/RPC/BitmaskBcoin/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.BLANK_RESULT = '';
-exports.ERROR_METHOD_NOT_IMPLEMENTED = 5;
-exports.ERROR_NOT_AUTHORIZED = 4;
-exports.ERROR_NONE = 0;
-exports.isRequest = function (msg) { return 'method' in msg.body; };
